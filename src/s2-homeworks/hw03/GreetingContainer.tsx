d0276451351e277c46c743ee6464669f03cefbbd
HlebSayanov <say4havi@gmail.com>
2022-12-03 14:59:59 +0300

    edit_HW3_lastName

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,50 +1,50 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
-import { UserType } from './HW3'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
     users: UserType[] // need to fix any
-    addUserCallback: (name: string)=>void // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
-export const pureAddUser = (name: string, setError: (e: string)=>void , setName: (name: string)=>void, addUserCallback: (name: string)=>void) => {
+export const pureAddUser = (name: string, setError: (e: string) => void, setName: (name: string) => void, addUserCallback: (name: string) => void) => {
     // если имя пустое - показать ошибку: setError('Ошибка! Введите имя!'),
     // иначе - добавить юзера при помощи addUserCallback и очистить инпут засетав ''
     // проверить на пустоту можно при помощи метода trim(). К примеру: name.trim() !== ''
     // ЕСЛИ НЕ БУДЕТ ПОЛУЧАТЬСЯ, НЕ РАССТРАИВАЙСЯ. НА ЧЕТВЕРТОМ ЗАНЯТИИ ПО ТУДУЛИСТУ НАУЧИМ), НО ВСЕ ТАКИ ПОПЫТАЙСЯ))
-    if(name.trim()===''){
+    if (name.trim() === '') {
         setError('Ошибка! Введите имя!')
-    }else{
+    } else {
         addUserCallback('new Name')
         setName('')
     }
 }
 
-export const pureOnBlur = (name: string, setError: (e: string)=>void) => { // если имя пустое - показать ошибку
+export const pureOnBlur = (name: string, setError: (e: string) => void) => { // если имя пустое - показать ошибку
     if (name === '') {
         setError('Ошибка! Введите имя!')
     }
 }
 
-export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: ()=>void) => { // если нажата кнопка Enter - добавить}
-    if(e.key==='Enter'){
-addUser()
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: () => void) => { // если нажата кнопка Enter - добавить}
+    if (e.key === 'Enter') {
+        addUser()
     }
-    }
+}
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
 // более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
+                                                                     users,
+                                                                     addUserCallback,
+                                                                 }) => {
     // деструктуризация пропсов
     const [name, setName] = useState<string>('') // need to fix any
     const [error, setError] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e:ChangeEvent<HTMLInputElement> ) => { // need to fix any
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
 
         error && setError('')
@@ -69,7 +69,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName = users[users.length-1]?.name
+    const lastUserName = totalUsers > 0 ? users[users.length - 1].name : ''
 
     // need to fix
 
