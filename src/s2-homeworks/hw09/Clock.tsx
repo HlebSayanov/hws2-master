9e31dbee439a7b0ddb0dbcafe246964e158e4fb1
HlebSayanov <say4havi@gmail.com>
2023-03-04 17:51:06 +0300

    add_HW9_edit

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -36,14 +36,14 @@ function Clock() {
     }
 
 
-    let days = ['Sunday ','Monday ', 'Tuesday ', 'Wednesday ', 'Thursday ', 'Friday ', 'Saturday'];
-    let months = ["January ", "February ", "March ", "April ", "May ", "June ",
-        "July ", "August ", "September ", "October ", "November ", "December "];
+    let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
 
     const stringTime = date.toLocaleTimeString() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = date.toLocaleDateString('ru-RU') || <br/>  // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
-
+    console.log(stringTime)
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = days[date.getDay()] || <br/> // пишут студенты
     const stringMonth = months[date.getMonth()] || <br/> // пишут студенты
